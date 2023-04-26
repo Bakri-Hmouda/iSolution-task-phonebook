4492b2c6dc91a3a01b57d29a6d26aad87f2a50b0
Bakrifly <bakri.hmouda@gmail.com>
2023-04-26 14:48:58 +0300

    added contact view template with route layout

--- apps/backend/resources/views/contacts.blade.php
+++ apps/backend/resources/views/contacts.blade.php
@@ -1,9 +1,44 @@
-<h1>contacts</h1>
-<p>welcome to phonebook</p>
+@extends("layouts.layout")
+
+@section("content")
+
+<h1>Welcome to phonebook</h1>
+<a href="/new" class="btn btn-primary" role="button">
+    new contact
+</a>
+<h4 class="mt-4">contacts</h4>
 
 
-<ul>
-    @foreach ($contacts->all() as $contact)
-    <li>{{ $contact->firstName }}</li>
-    @endforeach
-</ul>
+<div class="container">
+    @if(empty($contacts->all()))
+    <div class="d-flex justify-content-center">
+
+        <p class="h4 text-danger">no contact saved yet</p>
+    </div>
+    @else
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Number</th>
+                <th scope="col">Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($contacts->all() as $contact)
+            <tr>
+                <th scope="row">{{$contact->id}}</th>
+                <td>{{$contact->firstName}}</td>
+                <td>{{$contact->lastName}}</td>
+                <td>{{$contact->phoneNumber}}</td>
+                <td>{{$contact->email}}</td>
+
+            </tr>
+
+            @endforeach
+        </tbody>
+    </table>
+    @endif
+</div>
