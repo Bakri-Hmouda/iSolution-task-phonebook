c48a6b490ba461a87b56976f12fd34df01996d2b
Bakrifly <bakri.hmouda@gmail.com>
2023-04-26 14:49:53 +0300

    added bootstrap styling and a back button

--- apps/backend/resources/views/newContact.blade.php
+++ apps/backend/resources/views/newContact.blade.php
@@ -1,45 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends("layouts.layout")
+@section("content")
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<a href="/" class="btn btn-secondary mb-4" role="button">GO BACK</a>
+<h1>Create a new contact</h1>
+<form method="POST" action="/new">
+    @csrf
 
-<body>
-    <form method="POST" action="/new">
-        @csrf
-        <label for="firstName">
-            <input type="text" name="firstName" placeholder="First name">
-        </label>
-
-        <label for="lastName">
-            <input type="text" name="lastName" placeholder="Last name">
-        </label>
-
-        <label for="email">
-            <input type="email" name="email" placeholder="email">
-        </label>
-
-        <label for="phoneNumber">
-            <input type="text" name="phoneNumber" placeholder="Phone Number">
-        </label>
-
-        <button type="submit">Save</button>
-    </form>
-
-
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+    <div class="input-group mb-3">
+        <input class="form-control" type="text" name="firstName" placeholder="First name">
     </div>
-    @endif
-</body>
 
-</html>
+    <div class="input-group mb-3">
+        <input class="form-control" type="text" name="lastName" placeholder="Last name">
+    </div>
+
+    <div class="input-group mb-3">
+        <input type="email" class="form-control" placeholder="Email" aria-label="Recipient's username" aria-describedby="basic-addon2">
+    </div>
+
+    <div class="input-group mb-3">
+        <input class="form-control" type="text" name="phoneNumber" placeholder="Phone Number">
+    </div>
+
+    <button type="submit" class="btn btn-primary">SAVE</button>
+</form>
+
+
+@if ($errors->any())
+<div class="mt-3 alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
